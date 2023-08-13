9f3b301c4a4ff059d350dbbe0aa773ff267a7f20
jke94 <jcarracedo.dev@gmail.com>
2023-08-13 19:39:42 +0200

    Added references to VC++ directories, linker and libs.

--- clientdynamiclibrary/clientdynamiclibrary.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Operations.h"
-
-int main()
-{
-    std::cout << "Hello World!\n";
-
-    suma(2.4, 3.6);
-}
---
+++ clientdynamiclibrary/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Operations.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    std::cout << suma(2.4, 3.6) << std::endl;
+    std::cout << resta(2.4, 3.6) << std::endl;
+    std::cout << multiplicar(2.4, 3.6) << std::endl;
+    std::cout << dividir(2.4, 3.6) << std::endl;
+}
